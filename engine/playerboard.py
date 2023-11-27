450c6ed551dcc883de922433ef230b4539c57b29
console-beaver <rybalkin@stanford.edu>
2023-11-27 00:51:18 -0800

    display info to playerboard

--- engine/playerboard.py
+++ engine/playerboard.py
@@ -20,3 +20,17 @@ class PlayerBoard(Board):
         return self.move
 
     # call self.turn to make a turn given the input
+
+    ''' 0 : unoccupied
+        1 : frozen tile
+        2 : current controlled piece occupies this tile '''
+
+    def display_info(self):
+        out = self.occupancy.astype(int)
+
+        for r_off in range(len(self.piece_ori)):
+            for c_off in range(len(self.piece_ori)):
+                if self.piece_ori[r_off, c_off]:
+                    out[self.piece_r + r_off, self.piece_c + c_off] = 2
+
+        return out
