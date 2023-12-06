cdf1f22099f58981faeb32684c44211d987f9a82
console-beaver <rybalkin@stanford.edu>
2023-12-06 00:38:39 -0800

    wrote some stuff in q-learning script

--- algos/q-learning.py
+++ algos/q-learning.py
@@ -2,7 +2,7 @@
 q-learning.py
 
 This file implements the main algorithm for generating the policy for a given dataset.
-It uses q-learning and e-greedy to generate an output 
+It uses q-learning and e-greedy to generate an output
 
 usage:
 python3 q-learning.py <dataset>.csv <output>.policy
@@ -16,13 +16,17 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-NUM_ITERATIONS = 100000
-MAX_ROLLOUT = 100000
+from ..app_utils.app_utils import Bot, TetrisCanvas
+
+NUM_TOTAL_REPEATS = 5
+
+NUM_ITERATIONS = 10000000
+MAX_ROLLOUT = 100
 
 # parameters for Epsilon Greedy Exploration
-EPSILON = 0.4
+EPSILON = 0.5
 MIN_EPSILON = 0.1
-E_DECAY = 0.97
+E_DECAY = 0.95
 
 # Parameters for Q-Learning
 ALPHA = 0.001 # Learning rate
@@ -84,39 +88,52 @@ def main():
     output_filename = sys.argv[2]
     print("Starting precomputing...")
     sa_r, s_a, sa_sp, actions = precompute(dataset_filename) # NOTE: 1-indexed!
-    print("Finished precomputing.")
     num_s = max(s_a)
     num_a = len(actions)
     q = np.zeros((num_s + 1, num_a + 1)) # make everything 1-indexed for simplicity
-    print("\nStarting iterations...")
-    for _ in tqdm(range(NUM_ITERATIONS)):
-        # pick random first starting state
-        s = np.random.randint(1, q.shape[0]) # TODO: pick out of the 1 of 2 starting states
-        for _ in range(MAX_ROLLOUT): 
-            # No actions for a state means we have reached a dead-end
-            if s not in s_a:
-                break
-            
-            a = policy(q, s, s_a, e)
-            if (s, a) not in sa_sp:
-                print("\nwarning!")
-            # Take action, observe next state and reward
-            r, sp = take_action(s, a, sa_r, sa_sp) 
-            # Q-learning update rule
-            # print(ALPHA * (r + GAMMA * np.max(q[sp,:]) - q[s,a]))
-            q[s,a] += ALPHA * (r + GAMMA * np.max(q[sp,:]) - q[s,a])
-            # Move to the next state
-            s = sp
-        # Decay epsilon over time
-        e = max(MIN_EPSILON, e * 0.99)
 
-    # Generate policy and save towards value
-    final_p = np.argmax(q, axis=1)[1:] # throws out placeholder for 1st value
-    for i in range(len(final_p)):
-        if final_p[i] == 0:
-            final_p[i] = np.random.randint(1, q.shape[1])
-    np.savetxt(output_filename, final_p, delimiter=',', fmt='%s')
-    print("finished!")
+    for i in range(NUM_TOTAL_REPEATS):
+        print("Starting iterations ("+str(i+1)+'/'+str(NUM_TOTAL_REPEATS)+")...")
+        for _ in tqdm(range(NUM_ITERATIONS)):
+            # pick random first starting state
+            s = np.random.randint(1, q.shape[0]) # TODO: pick out of the 1 of 2 starting states
+            for _ in range(MAX_ROLLOUT):
+                # No actions for a state means we have reached a dead-end
+                if s not in s_a:
+                    break
 
-if __name__ == '__main__':  
+                a = policy(q, s, s_a, e)
+                if (s, a) not in sa_sp:
+                    print("\nwarning!")
+                # Take action, observe next state and reward
+                r, sp = take_action(s, a, sa_r, sa_sp)
+                # Q-learning update rule
+                # print(ALPHA * (r + GAMMA * np.max(q[sp,:]) - q[s,a]))
+                q[s,a] += ALPHA * (r + GAMMA * np.max(q[sp,:]) - q[s,a])
+                # Move to the next state
+                s = sp
+            # Decay epsilon over time
+            e = max(MIN_EPSILON, e * E_DECAY)
+
+        # Generate policy and save towards value
+        print('Generating policy...')
+        final_p = np.argmax(q, axis=1)[1:] # throws out placeholder for 1st value
+        bueno = 0
+        v = 0
+        for i in tqdm(range(len(final_p))):
+            if final_p[i] == 0:
+                final_p[i] = np.random.randint(1, q.shape[1])
+            else:
+                v += max(0, q[i, final_p[i]])
+                bueno += 1
+        print('We have', bueno, 'nonrandom actions, expected 437399')
+        print('Found mean best q of', v / bueno)
+        print('Writing file...')
+        np.savetxt(output_filename, final_p, delimiter=',', fmt='%s')
+        print('Beginning evaluation...')
+        testbot = Bot(output_filename)
+
+    print("Finished!")
+
+if __name__ == '__main__':
     main()
